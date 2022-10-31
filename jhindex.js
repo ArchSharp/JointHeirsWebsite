70a7e72576b67c7c57894127def6a4e55f2fe445
Raphael <raphael.tunji@yahoo.com>
2022-10-31 15:55:04 +0100

    update7

--- jhindex.js
+++ jhindex.js
@@ -1,23 +1,22 @@
 window.onscroll = function() {myFunction()};
 window.onload = function(){myLoader()};
 
-var sticky_top = document.getElementById('fixedcolour');
-var loader = document.getElementById('loader');
-var main = document.getElementById('main');
+const sticky_top = document.getElementById('fixedcolour');
+const loader = document.getElementById('loader');
+const main = document.getElementById('main');
+
 var sticky = sticky_top.offsetTop;
 var myvar;
 function myFunction() {
   if (window.pageYOffset >= sticky+10) {
     sticky_top.style.backgroundColor= "brown";
-    //sticky_top.style.marginTop = "0px;";
-    //sticky_top.style.width = "100vw";
   } else if(window.pageYOffset < sticky+10){
     sticky_top.style.backgroundColor= "";
   }
 }
 
 function myLoader(){
-  myvar = setTimeout(showPage, 7000);
+  myvar = setTimeout(showPage, 3000);
 }
 
 function showPage(){
